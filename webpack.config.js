7b87d62dcc71c67d2231574e9b09eb12b858ff79
timremmert <tbr23@scarletmail.rutgers.edu>
2018-04-12 13:13:53 -0400

    setup dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,31 +5,32 @@ module.exports = (env) => {
 
     console.log('env', env);
     return {
-        entry: './src/app.jsx',
-    output: {
-        path: path.join(__dirname,'public'),
-        filename:'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.jsx$/,
-            exclude: /node_modules/
-        }, 
-    {
-        test: /\.s?css$/,
-        use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-        ]
-    }]
+         entry: './src/app.jsx',
+         output: {
+            path: path.join(__dirname,'public', 'dist'),
+            filename:'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.jsx$/,
+                exclude: /node_modules/
+            }, 
+        {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
     },
     
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname,'public'),
-        historyApiFallback: true
-    }
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname,'public'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
     };
 };
