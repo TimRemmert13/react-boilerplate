f435d5c3ce45b30b81b9a83d66cea213dc6839df
timremmert <tbr23@scarletmail.rutgers.edu>
2018-04-12 11:11:15 -0400

    changes to homepage

--- src/app.jsx
+++ src/app.jsx
@@ -12,15 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-console.log(store.dispatch(addExpense({description: 'Waterbill', amount: 4500})));
-console.log(store.dispatch(addExpense({description: 'Gasbill', createdAt: 1000})));
-console.log(store.dispatch(addExpense({description: 'Waterbill', amount: 19500})));
-console.log(store.dispatch(setTextFilter('water')));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
